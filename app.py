1bda55d4544b19b453adc78e89bd03ab49da600c
Paul Jones <paul.jones@dyson.com>
2019-05-23 12:32:57 +0200

    Create AWS CDK Stack with Lambda deployment and Lambda Handler implementation

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
 
-from aws_cdk import cdk
+from aws_cdk import aws_lambda as lambda_, cdk
 
-from cdk-py-lambda.cdk-py-lambda_stack import PyStack
 
+class PyStack(cdk.Stack):
+
+    def __init__(self, app: cdk.App, id: str, **kwargs) -> None:
+        super().__init__(app, id)
+
+        with open('lambda_handler.py', encoding="utf8") as fp:
+            handler_code = fp.read()
+
+            lambdaFn = lambda_.Function(
+                self,
+                "InlineLambda",
+                code=lambda_.InlineCode(handler_code),
+                handler="index.main",
+                timeout=300,
+                runtime=lambda_.Runtime.PYTHON37
+            )
 
 app = cdk.App()
-PyStack(app, "cdk-py-lambda-cdk-1")
-
+PyStack(app, "cdk-py-lambda-cdk")
 app.run()
--- cdk-py-lambda/cdk-py-lambda_stack.py
+++ cdk-py-lambda/cdk-py-lambda_stack.py
@@ -1,9 +1,2 @@
-from aws_cdk import cdk
 
 
-class CdkPyLambdaStack(cdk.Stack):
-
-    def __init__(self, app: cdk.App, id: str, **kwargs) -> None:
-        super().__init__(app, id)
-
-        # The code that defines your stack goes here
